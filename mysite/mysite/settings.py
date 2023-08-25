76c9a0dadaa355dda1b83f8d3e40dff0646f5054
Gary <poppingary@gmail.com>
2023-08-24 22:31:17 -0400

    Database setup

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "poll",
+        "USER": "root",
+        "PASSWORD": "Root-123",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
